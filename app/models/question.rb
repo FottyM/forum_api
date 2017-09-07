146e48bc661c24afc50216afc3c67d31e293c265
Fortunat Mutunda <fotimutunda@gmail.com>
2017-09-07 20:24:26 +0300

    creade seeds for questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
   validates_presence_of :title, :body
-  validates_length_of :title,  in: 5..60
+  validates_length_of :title,  in: 3..60
   validates_length_of :body, in: 3..255
 
 end
