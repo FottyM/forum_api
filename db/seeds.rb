a96bbb1d56eaaa16608626fb9d9e00d2f8d592f2
Fortunat Mutunda <fotimutunda@gmail.com>
2017-09-08 13:17:27 +0300

    Insert fake data via rake task or seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,10 @@ require 'faker'
 count = 0
 20.times do
   question = Question.create!( title: Faker::Hipster.word, body: Faker::Lorem.characters(255))
-  @answer = Answer.new content: Faker::Lorem.words.join(",") , question_id: question.id
-  # 5.times do
-
-    @answer.save!
-  # end
+  5.times do
+    answer = Answer.new content: Faker::Lorem.words.join(" ") + "?" , question_id: question.id
+    answer.save!
+  end
   count = count + 1
 end
 
