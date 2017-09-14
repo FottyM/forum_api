a968862ea44c4e0ec44015abd2c9f72558fb7d58
Fortunat Mutunda <fotimutunda@gmail.com>
2017-09-15 02:43:21 +0300

    crud user, override devise_for to match routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-
+scope 'api' do
+  scope 'vi' do
+    devise_for :users
+  end
+end
   namespace 'api' do
     namespace 'v1' do
-      devise_for :users
+      resources :users
       resources :questions do
         resources :answers
       end
