bd782256f64b2280f875cf85241981c5c1c0bdaa
Fortunat Mutunda <fotimutunda@gmail.com>
2017-09-15 22:56:48 +0300

    Revert "Users"

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-scope 'api' do
-  scope 'vi' do
-    devise_for :users
-  end
-end
+
   namespace 'api' do
     namespace 'v1' do
-      resources :users
       resources :questions do
         resources :answers
       end
